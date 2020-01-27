e125a8c6ddf68a6002e0e86cdcf2c1848a2eff8f
BenHall-7 <benjaminjahall@gmail.com>
2020-01-26 19:22:53 -0500

    Add CSS

--- testing-app/src/comp/Dashboard.js
+++ testing-app/src/comp/Dashboard.js
@@ -3,10 +3,10 @@ import React from 'react';
 export default function Dashboard({addBall, addHit, addStrike, addFoul}) {
     return (
         <div id="dashboard">
+            <button onClick={addHit}>hit</button>
             <button onClick={addStrike}>strike</button>
             <button onClick={addBall}>ball</button>
             <button onClick={addFoul}>foul</button>
-            <button onClick={addHit}>hit</button>
         </div>
     )
 }
